bf171191effb4b15bfc250bb6b774fcb02a23988
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-11-30 19:15:08 +0300

    zad2_II

---
+++ zad2II/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad2II
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte num = 0;
+            byte.TryParse(textBox1.Text, out num);
+            switch (num)
+            {
+                case 1: MessageBox.Show("Пики"); break;
+                case 2: MessageBox.Show("Трефы"); break;
+                case 3: MessageBox.Show("Бубны"); break;
+                case 4: MessageBox.Show("Червы"); break;
+                default: MessageBox.Show("Неверное число! Попробуйте ещё раз"); break;
+            }
+        }
+    }
+}
