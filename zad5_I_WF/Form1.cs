2a7a6d7398d6e1ce134b86dab87a1015feaf0d03
J-I-L-L <95297721+J-I-L-L@users.noreply.github.com>
2021-12-07 19:41:38 +0300

    zad5_I_WF

---
+++ zad5_I_WF/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zad5_I_WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        static double f(double x)
+        {
+            try
+            {
+                if (x == 0)
+                {
+                    throw new Exception();
+                }
+                else
+                {
+                    return Math.Sqrt(x * x - 1);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tb1 = "";
+            double a = 0;
+            string tb2 = "";
+            double b = 0;
+            string tb3 = "";
+            double h = 0;
+            try
+            {
+                tb1 = textBox1.Text;
+                tb2 = textBox2.Text;
+                tb3 = textBox3.Text;
+                a = double.Parse(tb1);
+                b = double.Parse(tb2);
+                h = double.Parse(tb3);
+                for (double i=a; i<=b; i=i+h)
+                {
+                    try
+                    {
+                        result.Append("y ["+ i + "] = "+f(i)+"\n");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ошибка! Возможно в вычислении присутствует ноль и/или неверное число. Убираем лишнее...");
+                    }
+                    richTextBox1.Text = result.ToString();
+                }
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Введено неверное значение!");
+            }
+        }
+    }
+}
